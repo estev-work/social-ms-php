c22bf4ce44b7bc7c87b0f8ced7e8ac33cb53cc38
Alexei Gybchenko <alexey.g@pyrobyte.ru>
2024-02-18 20:12:13 +0700

    feat: Функционал валидации на основе дто

--- project/post-service/src/Requests/CreateNewPostRequest.php
+++ project/post-service/src/Requests/CreateNewPostRequest.php
@@ -6,11 +6,6 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class CreateNewPostRequest
 {
-    public function __invoke()
-    {
-        dd($this);
-    }
-
     public function __construct(
         #[Assert\NotBlank]
         #[Assert\Length(min: 10, max: 500)]
